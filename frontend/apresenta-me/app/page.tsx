8c0f02087e618991e050b7c38592adafc848758e
andreodev <developerandreo@gmail.com>
2026-06-23 20:56:01 -0400

    updated

--- frontend/apresenta-me/app/page.tsx
+++ frontend/apresenta-me/app/page.tsx
@@ -1,5 +1,7 @@
 import HomeGetPage from "@/modules/home/pages/home-get-page";
 
+//serve como ponto de entrada para a aplicação, e vai renderizar o componente HomeGetPage
+
 export default function Page() {
   return (
     <HomeGetPage />
---
+++ frontend/apresenta-me/service/api.ts
@@ -0,0 +1,2 @@
+//por o api service que vai ser usado para fazer as chamadas a api do backend 
+//usar o axios
